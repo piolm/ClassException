b917e57a002604ec7f90c0d81a744a8ed688cc4e
piolm <piolm.p.r.o16@gmail.com>
2022-04-28 18:50:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws MyException {
+	// write your code here
+        System.out.println("ща вас затроллю ");
+        throw new MyException("Trollge moment");
+    }
+}
